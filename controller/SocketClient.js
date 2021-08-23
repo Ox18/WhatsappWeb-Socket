107561ca4200e7caced82ce52b432d6ee35247fb
Alex <wilmer.delgadoalama09@gmail.com>
2021-08-23 07:21:20 +0200

    separate logic code

---
+++ controller/SocketClient.js
@@ -0,0 +1,57 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode');
+
+class SocketClient{
+    constructor(){
+      this.client = new Client();      
+    }
+    
+    static buildInstance(io){
+      const socketClient = new SocketClient();
+      const { client } = socketClient;
+      io.on("connection", function (socket) {
+        console.log("Alguien se ha conectado con Sockets");
+        
+        client.on('qr', (qr) => {
+          qrcode.toDataURL(qr, function(err, url){
+            console.log("se cambio el url");
+            socket.emit("messages", url);
+          })
+        });
+      
+        socket.on("new-message", function (data) {
+          messages.push(data);
+      
+          io.sockets.emit("messages", messages);
+        });
+      });
+      
+      client.on('authenticated', async (session) => {
+        console.log(session);
+          console.log('autenticado');
+      });
+      
+      client.on('message', async msg => {
+        msg.reply('hola');
+      })
+      
+      client.on('ready', () => {
+        console.log('Client is ready!');
+        console.log(client.info);
+      });
+      
+      
+      client.on('message', msg => {
+        if (msg.body == '!ping') {
+            msg.reply('pong');
+        }
+      });
+      
+      client.initialize();
+      
+
+      
+    }
+}
+
+module.exports = SocketClient;
