ec4b5a839b306d5cd4e6656a06e480afc4408808
Alex <wilmer.delgadoalama09@gmail.com>
2021-08-23 07:38:01 +0200

    fix css

--- public/main.js
+++ public/main.js
@@ -1,5 +1,7 @@
 var socket = io.connect("http://localhost:8080", { forceNew: true });
 
 socket.on("messages", function (data) {
-  document.querySelector("#qr_code").style.background="url('"+data+"') fixed"; 
+  document.querySelector("#qr_code").innerHTML = `
+    <img src="${data}" alt="qr" />
+  `
 });
