78d4a0da5abbe895751e14f6c525fd1fe89f8758
Wilmer Delgado <Wilmer.DelgadoAlama09@gmail.com>
2021-08-23 20:13:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,8 @@ var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 var SocketClient = require('./controller/SocketClient');
-
 const socketClient = new SocketClient(io);
-
 app.use(express.static("public"));
-
-app.get("/hello", function (req, res) {
-  res.status(200).send("Hello World!");
-});
 
 server.listen(8080, function () {
   console.log("Servidor corriendo en http://localhost:8080");
