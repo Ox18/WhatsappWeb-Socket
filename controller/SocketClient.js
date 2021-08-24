db465d0e1a9b17871ca1bf2d45ed2134295e060b
Alex <wilmer.delgadoalama09@gmail.com>
2021-08-24 03:24:23 +0200

    fix error

--- controller/SocketClient.js
+++ controller/SocketClient.js
@@ -1,55 +1,52 @@
-const { Client } = require('whatsapp-web.js');
-const qrcode = require('qrcode');
+const { Client } = require("whatsapp-web.js");
+const qrcode = require("qrcode");
 
-class SocketClient{
-    constructor(io){
-      this.io = io;
-      this.client = new Client();
-      this.qrURL = "";
-      this.initialize();
-    }
-    
-    initialize(){
-            this.io.on("connection", this.onConnectionIO);
-            this.client.on('authenticated', this.onAuthenticatedClient);
-            this.client.on('message', this.onMessageClient);
-            this.client.on('ready', this.onReadyClient);  
-            this.client.on('qr', this.onQrClient);
-            this.client.initialize();
-    }
-    
-    // Here you put all the tasks when the user first connects to the web site
-    onConnectionIO(socket){
-        socket.on("new-message", this.onNewMessageIO);
-    }
-    
-    // function that notifies when the user has been authenticated
-    async onAuthenticatedClient(session){
-        console.log('authenticated');
-    }
-    
-    // function that responds when the user sends a message to the bot
-    async onMessageClient(msg){
-        msg.reply('hello world!');
-    }
-    
-    // notifies you when the client instance is ready
-    onReadyClient(){
-        console.log('Client is ready!');
-    }
-    
-    // function that generates the qr code and send it to the web
-    onQrClient(qr){
-        var self = this;
-        qrcode.toDataURL(qr, function(err, url){
-            self.onNewMessageIO(url);
-        })
-    }
-    
-    // issue a new message when the user is newly logged in
-    onNewMessageIO(data){
-        this.io.sockets.emit("messages", this.qrURL || "");
-    }
+class SocketClient {
+  constructor() {
+    this.client = new Client();
+  }
+
+  static buildInstance(io) {
+    const socketClient = new SocketClient();
+    const { client } = socketClient;
+
+    io.on("connection", function (socket) {
+      console.log("Alguien se ha conectado con Sockets");
+
+      client.on("qr", (qr) => {
+        qrcode.toDataURL(qr, function (err, url) {
+          console.log("se cambio el url");
+          socket.emit("messages", url);
+        });
+      });
+
+      socket.on("new-message", function (data) {
+        messages.push(data);
+
+        io.sockets.emit("messages", messages);
+      });
+    });
+
+    client.on("authenticated", async (session) => {
+      console.log("autenticado");
+    });
+
+    client.on("message", async (msg) => {
+      msg.reply("hola");
+    });
+
+    client.on("ready", () => {
+      console.log("Client is ready!");
+    });
+
+    client.on("message", (msg) => {
+      if (msg.body == "!ping") {
+        msg.reply("pong");
+      }
+    });
+
+    client.initialize();
+  }
 }
 
 module.exports = SocketClient;
