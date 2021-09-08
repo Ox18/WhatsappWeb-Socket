313dcf351eac232c2028a0e3f5f454adc3b089c6
Alex <wilmer.delgadoalama09@gmail.com>
2021-09-08 17:55:39 -0500

    add clone chat

--- controller/SocketClient.js
+++ controller/SocketClient.js
@@ -4,6 +4,8 @@ const qrcode = require("qrcode");
 class SocketClient {
   constructor() {
     this.client = new Client();
+    this.qr = "https://img.pikbest.com/58pic/35/39/61/62K58PICb88i68HEwVnm5_PIC2018.gif!w340";
+    this.chat = {};
   }
 
   static buildInstance(io) {
@@ -11,39 +13,65 @@ class SocketClient {
     const { client } = socketClient;
 
     io.on("connection", function (socket) {
-      console.log("Alguien se ha conectado con Sockets");
+      socket.emit("messages", socketClient.qr);
+      socket.emit("initialChat", socketClient.chat);
 
       client.on("qr", (qr) => {
         qrcode.toDataURL(qr, function (err, url) {
-          console.log("se cambio el url");
+          socketClient.qr = url;
           socket.emit("messages", url);
         });
       });
 
-      socket.on("new-message", function (data) {
-        messages.push(data);
-
-        io.sockets.emit("messages", messages);
+      client.on("ready", () => {
+        const uri = "https://i.pinimg.com/originals/87/98/77/879877f9ddebaf63aa83976fe65a87ce.gif";
+        socket.emit("messages", uri);
+        socketClient.qr = uri;
       });
+
+      client.on("message", async (msg) => {
+        const { body, from, timestamp, deviceType } = msg;
+        const destinatary = from.replace("51", "").replace("@c.us", "");
+
+        if (socketClient.chat[destinatary] != undefined) {
+          // existe en el chat
+          socketClient.chat[destinatary].conversation.push({
+            time: timestamp,
+            device: deviceType,
+            message: body,
+            type: 'client'
+          });
+        } else {
+          // no existe en el chat
+          socketClient.chat[destinatary] = {
+            from: destinatary,
+            conversation: [
+              {
+                time: timestamp,
+                device: deviceType,
+                message: body,
+                type: 'client'
+              }
+            ]
+          };
+        }
+        socketClient.chat[destinatary].conversation.push({
+          time: Date.now(),
+          device: 'computer',
+          message: "hola",
+          type: 'bot'
+        });
+        // msg.reply("hola");
+        socket.emit("initialChat", socketClient.chat);
+      });
+
     });
 
     client.on("authenticated", async (session) => {
       console.log("autenticado");
     });
 
-    client.on("message", async (msg) => {
-      msg.reply("hola");
-    });
 
-    client.on("ready", () => {
-      console.log("Client is ready!");
-    });
-
-    client.on("message", (msg) => {
-      if (msg.body == "!ping") {
-        msg.reply("pong");
-      }
-    });
 
     client.initialize();
   }
